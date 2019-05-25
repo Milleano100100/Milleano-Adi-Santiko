3f3d3dab3846c421e775f70c00907a1e1f53b258
Milleano100100 <50965735+Milleano100100@users.noreply.github.com>
2019-05-25 08:45:04 +0700

    Add files via upload

---
+++ Tugas_Akhir_Milleano/src/tugas_akhir_milleano/kelasta.java
@@ -0,0 +1,35 @@
+package tugas_akhir_milleano;
+
+/**
+ *
+ * @author MILLEANO ADI SANTIKO
+ */
+public class kelasta {
+String tes;
+String tess;
+String tesss;
+
+public void setTes(String tes){
+this.tes = tes;
+}
+public void setTess(String tess){
+this.tess = tess;
+}
+public void setTesss(String tesss){
+this.tesss = tesss;
+}
+
+public String getTes(){
+return tes;
+}
+public String getTess(){
+return tess;
+}
+public String getTesss(){
+return tesss;
+}
+
+    String getText() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
